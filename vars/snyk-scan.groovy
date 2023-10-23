fc522082ac0b06d2a820639eec03a08de939d6db
ANANTH KANNAN <2459461+anboss@users.noreply.github.com>
2023-10-22 21:53:15 -0400

    Update snyk-scan.groovy

--- vars/snyk-scan.groovy
+++ vars/snyk-scan.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call() {
+def snykscan() {
        stage('SnykScan'){
             steps{
                 snykSecurity failOnError: false, failOnIssues: false, organisation: 'anboss', projectName: 'kjap-platform', snykInstallation: 'MySnyk', snykTokenId: 'abf17a73-719c-40d5-97b5-dffad3856df7'
