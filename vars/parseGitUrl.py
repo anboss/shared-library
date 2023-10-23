19f27630bef1634f21ea81c5b8ef00aacb59b759
Ananth Kannan <2459461+anboss@users.noreply.github.com>
2023-10-23 19:35:58 -0400

    parse git url

--- vars/parseGitUrl.py
+++ vars/parseGitUrl.py
@@ -1,16 +1,11 @@
-import re
+import re, sys
 
 #extract project and namespace from project url
-def extract_fields_from_repo_url(repository_url):
+def extract(repository_url):
   matches = re.search(r'([a-z]+):\/\/([^/]*)\/([^/]*)\/(.*)\.git', repository_url)
   protocol = matches.group(1)
   domain = matches.group(2)
   name_space = matches.group(3)
   project = matches.group(4)
-
-  return name_space, project
-
-name_space, project = extract_fields_from_repo_url('https://gitlab.com/gitlab-org/gitlab-ce.git')
-
-print (name_space)
-print (project)
+  print(name_space)
+  sys.exit(name_space)
