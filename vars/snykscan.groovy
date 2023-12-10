a71f3c8e64d11d7f0c418f949f9216629c2df364
ANANTH KANNAN <2459461+anboss@users.noreply.github.com>
2023-12-09 22:10:00 -0500

    Update snykscan.groovy

--- vars/snykscan.groovy
+++ vars/snykscan.groovy
@@ -3,10 +3,11 @@
 def call(projectName, organisation) {
        echo "projectName - $projectName"
        echo "organisation - $organisation"
+	
        //snykSecurity failOnError: false, failOnIssues: false, organisation: organisation, projectName: projectName, snykInstallation: 'MySnyk', snykTokenId: 'snyk-token'
 
       script {
-	       sh 'snyk code test'
+	       sh 'C:/Gradle/bin/snyk code test'
       }
        
        snykSecurity (
