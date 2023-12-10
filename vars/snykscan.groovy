cd94c2a49b2fbc824febd5f383e062920f214b9e
ANANTH KANNAN <2459461+anboss@users.noreply.github.com>
2023-12-09 22:49:04 -0500

    Update snykscan.groovy

--- vars/snykscan.groovy
+++ vars/snykscan.groovy
@@ -10,13 +10,13 @@ def call(projectName, organisation) {
 	       	sh 'C:/Gradle/bin/snyk code monitor --all-projects'
       }
        
-       snykSecurity (
-              failOnError: false, 
-              failOnIssues: false, 
-              organisation: organisation, 
-              projectName: projectName, 
-              snykInstallation: 'MySnyk', 
-              snykTokenId: 'snyk-token',
-              additionalArguments: '--code -debug'   
-       )
+       // snykSecurity (
+       //        failOnError: false, 
+       //        failOnIssues: false, 
+       //        organisation: organisation, 
+       //        projectName: projectName, 
+       //        snykInstallation: 'MySnyk', 
+       //        snykTokenId: 'snyk-token',
+       //        additionalArguments: '--code -debug'   
+       // )
 }
